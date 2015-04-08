74a79343d9e91ab83ced122e49cb1a24f6e40363
Bastien Naru <aurchrome@live.fr>
2015-04-08 23:24:48 +0200

    Traduction + Images slider + Couleur boutons

---
+++ proposer.php
@@ -0,0 +1,79 @@
+
+
+<div class="row">
+			<div class="col-xs-12 col-sm-6">
+				<h1>Proposer une application</h1><p>Commencez à décrire votre projet d'application.</p><form enctype="multipart/form-data" name="registration" class="registration" method="post">
+<div class="slideDatas">
+<div class="form-group app_title">
+<label for="app_title">* Nom de votre application</label>
+<input class="form-control" type="text" name="app_title" id="app_title" required="required" maxlength="64" placeholder="* Nom de votre application">
+</div>
+<div class="g_id">
+<label for="g_id">Catégorie</label>
+<select name="g_id" id="g_id" required="required">
+<option value="">Choisir une catégorie</option>
+<option value="2">Actualités</option>
+<option value="1">Affaires</option>
+<option value="3">Développeurs</option>
+<option value="4">Divertissement</option>
+<option value="5">Education/ Enseignement</option>
+<option value="6">Finances</option>
+<option value="25">Fond D Ecran</option>
+<option value="7">Forme &amp; Santé</option>
+<option value="8">Graphisme Et Design</option>
+<option value="9">Livres</option>
+<option value="10">Loisirs</option>
+<option value="11">Médecine</option>
+<option value="24">Médecine</option>
+<option value="23">Météo</option>
+<option value="13">Multimédia Et Video</option>
+<option value="12">Musique Et Audio</option>
+<option value="18">Personnalisation</option>
+<option value="14">Photographie</option>
+<option value="15">Productivité</option>
+<option value="17">Professionnel</option>
+<option value="19">Référence</option>
+<option value="16">Réseaux Sociaux</option>
+<option value="26">Shopping</option>
+<option value="20">Social</option>
+<option value="21">Sports</option>
+<option value="27">Style De Vie</option>
+<option value="28">Transport Navigation</option>
+<option value="22">Utilitaires</option>
+</select></div>
+</div>
+<div class="slideDatas">
+<div class="form-group app_short_descro">
+<label class="txtArea" for="app_short_descro">* Description de votre application</label>
+<textarea class="form-control" name="app_short_descro" id="app_short_descro" required="required"></textarea></div>
+</div>
+<div class="slideDatas">
+	<div class="form-group app_platform">
+		<label>Sélectionnez une plate-forme</label><br><input type="checkbox" value="1" id="app_platform_1" name="app_platform[]">
+		<label for="app_platform_1">Apps Store (Apple iOS)</label><br><input type="checkbox" value="2" id="app_platform_2" name="app_platform[]">
+		<label for="app_platform_2">Google Play (Google Androïd)</label><br><input type="checkbox" value="3" id="app_platform_3" name="app_platform[]">
+		<label for="app_platform_3">Windows Phone (Microsoft Store)</label><br>
+	</div>
+</div>
+<div class="slideDatas">
+	<div class="form-group app_platform">
+		<label>Gestion du projet</label><br>
+		<input type="radio" value="1" id="app_manage_1" name="app_manage">
+		<label for="app_manage_1">Je gére mon projet et je suis propriétaire de l'application</label><br>
+		<input type="radio" value="2" id="app_manage_2" name="app_manage">
+		<label for="app_manage_2">Kaaze s'occupe du projet et je suis associé</label>
+	</div>
+	<div class="form-group app_goal">
+		<label for="app_goal">Montant à atteindre</label>
+		<input class="form-control" type="text" name="app_goal" id="app_goal" maxlength="10" placeholder="Montant à atteindre">
+	</div>
+</div>
+<div class="btn-submit-form">
+<input class="btn btn-default" type="submit" name="submit" id="submit" value="envoyer">
+</div>
+</form>
+			</div>
+			<div class="col-xs-12 col-sm-6">
+				<!-- content here -->
+			</div>
+		</div>
